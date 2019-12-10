92f201db8789f81793c0b33c88bf7795a5c7b6e8
Fiona Rose <fiona.r.murphey@gmail.com>
2019-12-10 18:58:14 -0500

    animation troubles

---
+++ exercises/final/Assets/Scripts/PlayerAnimationScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationScript : MonoBehaviour
+{
+    // References Setup
+    public Animator animator;
+    public bool WheelieAnimationIsPlaying = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Wheelie on W (handled in animation script)
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetTrigger("WheelieActivated");
+        }
+    }
+}
--- exercises/final/Assets/Scripts/PlayerMovementScript.cs
+++ exercises/final/Assets/Scripts/PlayerMovementScript.cs
@@ -7,8 +7,9 @@ public class PlayerMovementScript : MonoBehaviour
     public GameManager gm;
     public Renderer PlayerBodyRend;
     public GameObject PlayerBody;
-    public Animator animator;
+    public GameObject PlayerModel;
     public bool colorCycle;
+    bool checkWheelie;
     float colorPosition = 0;
     float colorChangeSpeed;
 
@@ -20,11 +21,10 @@ public class PlayerMovementScript : MonoBehaviour
     public float turnSpd;
     // set MAXSPD, MINSPD, moveSpd, turnSpd - also public (change in Unity editor)
 
-    // Jumping Setup
-    public bool WheelieAnimationIsPlaying = false;
-    float yVelocity = 0;
-    public float jumpForce = 2.5f;
-    public float gravityModifier = 0.025f;
+    //// Jumping Setup
+    //float yVelocity = 0;
+    //public float jumpForce = 2.5f;
+    //public float gravityModifier = 0.025f;
     bool prevIsGrounded;
 
     // Collision Handling
@@ -40,6 +40,8 @@ public class PlayerMovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        checkWheelie = GetComponent<PlayerAnimationScript>().WheelieAnimationIsPlaying;
+
         colorChangeSpeed = moveSpd / (throttle * 1.5f);
 
         if (colorCycle)
@@ -48,7 +50,6 @@ public class PlayerMovementScript : MonoBehaviour
             PlayerBodyRend.material.color = Color.HSVToRGB(colorPosition % 1.0f, 0.8f, 1);
         }
 
-        cc.Move(transform.forward * moveSpd * Time.deltaTime);
 
         // if LEVELSTART=true, set SPD=MINSPD
         if (gm.lvlStart)
@@ -92,11 +93,13 @@ public class PlayerMovementScript : MonoBehaviour
             transform.Rotate(0, turnSpd * Time.deltaTime, 0);
         }
 
-        // Wheelie on W
-        if(Input.GetKeyDown(KeyCode.W))
-        {
-            animator.SetTrigger("WheelieActivated");
-        }
+        // Wheelie on W (handled in animation script)
+        //if(Input.GetKeyDown(KeyCode.W))
+        //{
+        //    animator.SetTrigger("WheelieActivated");
+        //}
+
+        cc.Move(transform.forward * moveSpd * Time.deltaTime);
 
         //// jump on W (+ add animations)
         //if (cc.isGrounded)
@@ -142,7 +145,7 @@ public class PlayerMovementScript : MonoBehaviour
         if (other.gameObject.CompareTag("Obstacle"))
         {
             collideFlag = true;
-            if (collideFlag)
+            if (collideFlag && checkWheelie)
             {
                 gm.Lives -= 1;
                 gm.SetLivesText();
